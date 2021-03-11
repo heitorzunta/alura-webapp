514d4e7f7e70d3ee95d2bf8e2bc53bdfb4657e5a
Heitor Batistela Zunta <heitor@basenetworks.com.br>
2021-03-11 10:45:10 -0400

    Tentando arrumar o erro ao importar a classe Artigo.php dentro do arquivo: adicionar-artigo.php

--- src/Artigo.php
+++ src/Artigo.php
@@ -18,12 +18,20 @@ class Artigo {
 	}
 
 	public function encontrarCorpoArtigo(string $id): array{
-		$sql = 'SELECT id, titulo, conteudo FROM artigos WHERE id LIKE ?';
 
+		# Usando a sintaxe anti mysql injection
+		$sql = 'SELECT id, titulo, conteudo FROM artigos WHERE id LIKE ?';
 		$consultaArtigo = $this->mysql->prepare($sql);
 		$consultaArtigo->bind_param('s', $id);
 		$consultaArtigo->execute();
 		$artigo = $consultaArtigo->get_result()->fetch_assoc();
 		return $artigo;
+	}
+
+	public function adicionarArtigo(string $titulo, string $conteudo): void{
+		$sql = 'INSERT INTO artigo (titulo, conteudo) VALUES (?,?)';
+		$requisicao = $this->mysql->prepare($sql);
+		$requisicao->bind_param('ss', $titulo, $conteudo);
+		$requisicao->execute();
 	}
 }
